90efb6237202aba579697f66d3e7825c97f0b933
Bharadwaj16555 <bharadwajrhl@gmail.com>
2025-07-25 16:13:12 +0530

    My React Project Successful

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <center>
+      <h1>"Hello World Vite App"</h1>
+      <h2>"This is a simple Vite + React application."</h2>
+      </center>
+    </>
+  )
+}
+
+export default App
+
+
+
